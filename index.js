95511caec236f047f92148315fefa004a20c1467
Jeevan James <jeevanjj@live.com>
2017-06-15 15:52:18 +0530

    Added new module to generate the model factory class.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 module.exports = {
     buildHeader: require('./lib/build-header'),
+    buildModelFactoryClass: require('./lib/build-model-factory-class'),
     buildOperationDocComments: require('./lib/build-operation-doc-comments'),
     getDataType: require('./lib/get-data-type'),
     getMethodSignature: require('./lib/get-method-signature'),
